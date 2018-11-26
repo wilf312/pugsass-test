0464700338d1ca79fdc02883735c95e47e7891c6
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-11-26 22:48:38 +0900

    add

---
+++ src/components/test.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div.aaaa
+    | item
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.aaaa {
+  background: black;
+  color: white;
+}
+</style>
